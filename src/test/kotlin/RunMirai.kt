8288228c5b535a3f21bbaf8c48d431aea1281afa
Cian <topjohncian@gmail.com>
2021-02-04 12:43:31 +0800

    Feat: RunMirai 使用环境变量

--- src/test/kotlin/RunMirai.kt
+++ src/test/kotlin/RunMirai.kt
@@ -1,20 +1,25 @@
-package org.example.mirai.plugin
+package com.poicraft.bot.v4.plugin
 
 import net.mamoe.mirai.alsoLogin
 import net.mamoe.mirai.console.MiraiConsole
 import net.mamoe.mirai.console.plugin.PluginManager.INSTANCE.enable
 import net.mamoe.mirai.console.plugin.PluginManager.INSTANCE.load
 import net.mamoe.mirai.console.terminal.MiraiConsoleTerminalLoader
+import net.mamoe.mirai.console.util.ConsoleExperimentalApi
 
+@ConsoleExperimentalApi
 suspend fun main() {
     MiraiConsoleTerminalLoader.startAsDaemon()
 
     PluginMain.load()
     PluginMain.enable()
 
-    val bot = MiraiConsole.addBot(123456, "") {
+    val env = System.getenv()
+    val bot = env["ID"]?.let {
+        MiraiConsole.addBot(it.toLong(), env["PASSWORD"]!!) {
         fileBasedDeviceInfo()
     }.alsoLogin()
+    }
 
     MiraiConsole.job.join()
 }
