6f8e5b2d42d3fb1cc0a2b560f52ff515d38eeb85
gggxbbb <2331490629@qq.com>
2021-02-06 22:43:23 +0800

    Add: Permission

---
+++ src/main/kotlin/utils/Permission.kt
@@ -0,0 +1,8 @@
+package com.poicraft.bot.v4.plugin.utils
+
+@Suppress("unused", "PropertyName")
+object Permission {
+    const val PERMISSION_LEVEL_EVERYONE = 0
+    const val PERMISSION_LEVEL_ADMIN = 1
+    const val PERMISSION_LEVEL_OWNER = 2
+}
