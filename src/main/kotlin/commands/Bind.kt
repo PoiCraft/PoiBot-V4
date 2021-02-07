1c661cc51df2f61ebdf9b86313ed6543632df37a
gggxbbb <2331490629@qq.com>
2021-02-07 20:34:32 +0800

    Fix: 不再允许多次绑定

--- src/main/kotlin/commands/Bind.kt
+++ src/main/kotlin/commands/Bind.kt
@@ -7,7 +7,8 @@ import com.poicraft.bot.v4.plugin.database.DatabaseManager
 import com.poicraft.bot.v4.plugin.database.Users
 import com.poicraft.bot.v4.plugin.utils.Status
 import net.mamoe.mirai.event.events.GroupMessageEvent
-import org.ktorm.dsl.insert
+import net.mamoe.mirai.message.data.MessageSource.Key.quote
+import org.ktorm.dsl.*
 import java.time.Instant
 
 object Bind : Command() {
@@ -23,13 +24,23 @@ object Bind : Command() {
     override val argsRequired: Int = 1
 
     override suspend fun handleMessage(event: GroupMessageEvent, args: List<String>) {
-        DatabaseManager.instance().insert(Users) {
-            set(it.XboxID, args[1])
-            set(it.QQNumber, event.sender.id.toString())
-            set(it.CreateTime, Instant.now().epochSecond.toString())
-            set(it.Status, Status.NOT_VERIFIED.ordinal)
-        }
+        if (DatabaseManager.instance().from(Users)
+                .select(Users.QQNumber)
+                .where {
+                    ((Users.QQNumber eq event.sender.id.toString())
+                        or (Users.XboxID eq args[1]))
+                }.totalRecords == 0
+        ) {
+            DatabaseManager.instance().insert(Users) {
+                set(it.XboxID, args[1])
+                set(it.QQNumber, event.sender.id.toString())
+                set(it.CreateTime, Instant.now().epochSecond.toString())
+                set(it.Status, Status.NOT_VERIFIED.ordinal)
+            }
 
-        event.subject.sendMessage("已绑定 ${args[1]}")
+            event.subject.sendMessage(event.source.quote()+"已绑定 ${args[1]}")
+        } else {
+            event.subject.sendMessage(event.source.quote()+"此XboxID或QQ号已被绑定")
+        }
     }
 }
