d2a530cd5395a89d7152c4b7b9fa56b455178747
Cian <topjohncian@gmail.com>
2021-02-08 22:42:13 +0800

    Edit: QQ号应为 Long 类型

--- src/main/kotlin/database/User.kt
+++ src/main/kotlin/database/User.kt
@@ -3,12 +3,13 @@ package com.poicraft.bot.v4.plugin.database
 import org.ktorm.entity.Entity
 import org.ktorm.schema.Table
 import org.ktorm.schema.int
+import org.ktorm.schema.long
 import org.ktorm.schema.varchar
 
 @Suppress("PropertyName", "unused")
 interface User: Entity<User> {
     var XboxID: String
-    var QQNumber: String
+    var QQNumber: Long
     var Status: Int
     var CreateTime: String
 }
@@ -17,5 +18,5 @@ object Users: Table<User>("poi_users") {
     val CreateTime = varchar("create_time").bindTo { it.CreateTime }
     val Status = int("status").bindTo { it.Status }
     val XboxID = varchar("xbox_id").bindTo { it.XboxID }
-    val QQNumber = varchar("qq_number").bindTo { it.QQNumber }
+    val QQNumber = long("qq_number").bindTo { it.QQNumber }
 }
