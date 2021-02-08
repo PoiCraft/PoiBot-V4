551cd788807c4ec5532ce73079263006f7ec0517
gggxbbb <2331490629@qq.com>
2021-02-08 09:58:18 +0800

    Add: 添加解除绑定

--- src/main/kotlin/commands/Bind.kt
+++ src/main/kotlin/commands/Bind.kt
@@ -5,8 +5,10 @@ package com.poicraft.bot.v4.plugin.commands
 import com.poicraft.bot.v4.plugin.Command
 import com.poicraft.bot.v4.plugin.database.DatabaseManager
 import com.poicraft.bot.v4.plugin.database.Users
+import com.poicraft.bot.v4.plugin.utils.Permission
 import com.poicraft.bot.v4.plugin.utils.Status
 import net.mamoe.mirai.event.events.GroupMessageEvent
+import net.mamoe.mirai.message.data.At
 import net.mamoe.mirai.message.data.MessageSource.Key.quote
 import org.ktorm.dsl.*
 import java.time.Instant
@@ -18,29 +20,61 @@ object Bind : Command() {
         "bind"
     )
 
-    override val introduction: String = """绑定 XboxID 与 QQ.
-        |需提供参数 XboxID""".trimMargin()
+    override val introduction: String = """管理 QQ 与 Xbox ID 的绑定关系
+        |#bind set <XboxID> 以绑定 Xbox ID""".trimMargin()
 
-    override val argsRequired: Int = 1
+    override val enableSubCommand: Boolean = true
 
-    override suspend fun handleMessage(event: GroupMessageEvent, args: List<String>) {
-        if (DatabaseManager.instance().from(Users)
-                .select(Users.QQNumber)
-                .where {
-                    ((Users.QQNumber eq event.sender.id.toString())
-                        or (Users.XboxID eq args[1]))
-                }.totalRecords == 0
-        ) {
-            DatabaseManager.instance().insert(Users) {
-                set(it.XboxID, args[1])
-                set(it.QQNumber, event.sender.id.toString())
-                set(it.CreateTime, Instant.now().epochSecond.toString())
-                set(it.Status, Status.NOT_VERIFIED.ordinal)
+    object Add : Command() {
+        override val name: String = "新绑定"
+        override val argsRequired: Int = 1
+        override val aliases: List<String> = listOf(
+            "set"
+        )
+
+        override suspend fun handleMessage(event: GroupMessageEvent, args: List<String>) {
+            if (DatabaseManager.instance().from(Users)
+                    .select(Users.QQNumber)
+                    .where {
+                        ((Users.QQNumber eq event.sender.id.toString())
+                            or (Users.XboxID eq args[1]))
+                    }.totalRecords == 0
+            ) {
+                DatabaseManager.instance().insert(Users) {
+                    set(it.XboxID, args[1])
+                    set(it.QQNumber, event.sender.id.toString())
+                    set(it.CreateTime, Instant.now().epochSecond.toString())
+                    set(it.Status, Status.NOT_VERIFIED.ordinal)
+                }
+
+                event.subject.sendMessage(event.source.quote() + "已绑定 ${args[1]}")
+            } else {
+                event.subject.sendMessage(event.source.quote() + "此XboxID或QQ号已被绑定")
             }
-
-            event.subject.sendMessage(event.source.quote()+"已绑定 ${args[1]}")
-        } else {
-            event.subject.sendMessage(event.source.quote()+"此XboxID或QQ号已被绑定")
         }
     }
+
+    object Remove : Command() {
+        override val name: String = "消绑定"
+        override val argsRequired: Int = 1
+        override val aliases: List<String> = listOf(
+            "del"
+        )
+        override val permissionLevel: Permission = Permission.PERMISSION_LEVEL_ADMIN
+        override suspend fun handleMessage(event: GroupMessageEvent, args: List<String>) {
+            val target = args[1]
+                .replace("[mirai:at:", "")
+                .replace("]", "")
+                .replace("@", "")
+            DatabaseManager.instance().delete(Users) { it.QQNumber eq target }
+            event.subject.sendMessage(event.source.quote() + "绑定已解除" + target)
+        }
+    }
+
+    init {
+        newSubCommand(Add)
+        newSubCommand(Remove)
+    }
+
+
 }
