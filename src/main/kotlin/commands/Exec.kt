9362271f2553161e62849bdb70fb1c4556c1b85d
Cian <topjohncian@gmail.com>
2021-02-13 21:32:16 +0800

    Feat: 增加 RandomTP (#1)

--- src/main/kotlin/commands/Exec.kt
+++ src/main/kotlin/commands/Exec.kt
@@ -3,9 +3,15 @@ package com.poicraft.bot.v4.plugin.commands
 import com.poicraft.bot.v4.plugin.Command
 import com.poicraft.bot.v4.plugin.constants.Permission
 import com.poicraft.bot.v4.plugin.remote.bdxws.BDXWSControl
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import net.mamoe.mirai.event.events.GroupMessageEvent
 import net.mamoe.mirai.message.data.MessageSource.Key.quote
 
+/**
+ * 执行命令
+ * @see Command
+ * @author topjohncian
+ */
 object Exec : Command() {
     /**
      * 命令的人类友好名称
@@ -23,6 +29,7 @@ object Exec : Command() {
 
     override val argsRequired: Int = 1
 
+    @ExperimentalCoroutinesApi
     override suspend fun handleMessage(event: GroupMessageEvent, args: List<String>) {
         val result = BDXWSControl.runCmd(args[1])
 
