38b6dabf0fc56332744cb00ea4da3d46fa4e852b
gggxbbb <2331490629@qq.com>
2021-08-13 13:24:55 +0800

    Feat: 完善 DSL 命令声明

--- src/main/kotlin/plugins/exec.kt
+++ src/main/kotlin/plugins/exec.kt
@@ -1,8 +1,6 @@
 package com.poicraft.bot.v4.plugin.plugins
 
-import com.poicraft.bot.v4.plugin.by
-import com.poicraft.bot.v4.plugin.run
-import com.poicraft.bot.v4.plugin.command
+import com.poicraft.bot.v4.plugin.*
 import com.poicraft.bot.v4.plugin.constants.Permission
 import com.poicraft.bot.v4.plugin.remote.bdxws.BDXWSControl
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -16,12 +14,8 @@ fun B.exec() {
     /**
      * 执行任意命令
      */
-    command("执行命令") by "exec" run {
-        intro("执行命令")
-        require(Permission.PERMISSION_LEVEL_ADMIN)
-        onMessage { event, args ->
-            val result = BDXWSControl.runCmd(args.subList(1, args.size).joinToString(" "))
-            event.subject.sendMessage(event.source.quote() + "执行完成\n" + result)
-        }
+    command("执行命令") by "exec" intro "执行命令" require Permission.PERMISSION_LEVEL_ADMIN run { event, args ->
+        val result = BDXWSControl.runCmd(args.subList(1, args.size).joinToString(" "))
+        event.subject.sendMessage(event.source.quote() + "执行完成\n" + result)
     }
 }
