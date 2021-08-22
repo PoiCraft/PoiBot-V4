c58301284a23371fe283f60d4d9cacb4853db938
gggxbbb <2331490629@qq.com>
2021-08-22 15:50:07 +0800

    Feat: 使用现代方法构造白名单

--- src/main/kotlin/plugins/whitelist.kt
+++ src/main/kotlin/plugins/whitelist.kt
@@ -2,6 +2,8 @@ package com.poicraft.bot.v4.plugin.plugins
 
 import com.poicraft.bot.v4.plugin.by
 import com.poicraft.bot.v4.plugin.to
+import com.poicraft.bot.v4.plugin.require
+import com.poicraft.bot.v4.plugin.run
 import com.poicraft.bot.v4.plugin.command
 import com.poicraft.bot.v4.plugin.constants.Permission
 import com.poicraft.bot.v4.plugin.constants.WhitelistStatus
@@ -17,20 +19,16 @@ fun B.whitelist() {
      * 添加白名单
      * @author gggxbbb
      */
-    command("添加白名单") by "addw" to {
-        intro("添加白名单")
-        require(Permission.PERMISSION_LEVEL_ADMIN)
-        onMessage { event, args ->
-            val target = args.subList(1, args.size)
-            if (target.isEmpty()) {
-                event.subject.sendMessage("请提供 Xbox ID !")
-            } else {
-                val (status, result) = Whitelist.add(target.joinToString(" "))
-                when (status) {
-                    WhitelistStatus.PLAYER_ALREADY_IN_WHITELIST -> event.subject.sendMessage("玩家已在白名单中")
-                    WhitelistStatus.PLAY_ADDED -> event.subject.sendMessage("已添加至白名单")
-                    else -> event.subject.sendMessage("发生未知错误 $result")
-                }
+    command("添加白名单") by "addw" require Permission.PERMISSION_LEVEL_ADMIN run { event, args ->
+        val target = args.subList(1, args.size)
+        if (target.isEmpty()) {
+            event.subject.sendMessage("请提供 Xbox ID !")
+        } else {
+            val (status, result) = Whitelist.add(target.joinToString(" "))
+            when (status) {
+                WhitelistStatus.PLAYER_ALREADY_IN_WHITELIST -> event.subject.sendMessage("玩家已在白名单中")
+                WhitelistStatus.PLAY_ADDED -> event.subject.sendMessage("已添加至白名单")
+                else -> event.subject.sendMessage("发生未知错误 $result")
             }
         }
     }
@@ -39,20 +37,16 @@ fun B.whitelist() {
      * 添加白名单
      * @author gggxbbb
      */
-    command("删除白名单") by "rmw" to {
-        intro("删除白名单")
-        require(Permission.PERMISSION_LEVEL_ADMIN)
-        onMessage { event, args ->
-            val target = args.subList(1, args.size)
-            if (target.isEmpty()) {
-                event.subject.sendMessage("请提供 Xbox ID !")
-            } else {
-                val (status, result) = Whitelist.remove(target.joinToString(" "))
-                when (status) {
-                    WhitelistStatus.PLAYER_NOT_IN_WHITELIST -> event.subject.sendMessage("玩家不在白名单中")
-                    WhitelistStatus.PLAY_REMOVED -> event.subject.sendMessage("已从白名单中移除")
-                    else -> event.subject.sendMessage("发生未知错误 $result")
-                }
+    command("删除白名单") by "rmw" require Permission.PERMISSION_LEVEL_ADMIN run { event, args ->
+        val target = args.subList(1, args.size)
+        if (target.isEmpty()) {
+            event.subject.sendMessage("请提供 Xbox ID !")
+        } else {
+            val (status, result) = Whitelist.remove(target.joinToString(" "))
+            when (status) {
+                WhitelistStatus.PLAYER_NOT_IN_WHITELIST -> event.subject.sendMessage("玩家不在白名单中")
+                WhitelistStatus.PLAY_REMOVED -> event.subject.sendMessage("已从白名单中移除")
+                else -> event.subject.sendMessage("发生未知错误 $result")
             }
         }
     }
